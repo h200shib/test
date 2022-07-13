2dc45bf5a035fa627578ff00189740ad3e9ba361
h200shib <108774835+h200shib@users.noreply.github.com>
2022-07-13 11:13:53 +0900

    Update a.php

--- a.php
+++ a.php
@@ -2,7 +2,7 @@
 <?php 
   foreach(file('/usr/share/dict/linux.words') as $line){
     // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
-    if( strpos($line, "abc") ){ // strpos()関数で$lineにabcが含まれていればtrueを返す。
+    if( strpos($line, "abc") !== false ){ // strpos()関数で$lineにabcが含まれていればtrueを返す。
       echo "$line";
     }
   } 
